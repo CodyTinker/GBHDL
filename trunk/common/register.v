8f65eb56a2c671f3a4aff94e6b84eaa772111565
codytinker <codywtinker@gmail.com>
2016-08-12 22:33:55 -0400

    Added documentation header to all current files.

--- trunk/common/register.v
+++ trunk/common/register.v
@@ -3,10 +3,11 @@
 //  Module Name  : register.v                                                     //
 //  Create Date  : August, 2016                                                   //
 //  Author       : Kevin Millar                                                   //
+//                 Cody Tinker                                                    //
 //                                                                                //
-//  Description: Generic register file.                                           //
+//  Description:   Generic register file.                                           //
 //                                                                                //
-//  Dependencies: None                                                            //
+//  Dependencies:  None                                                            //
 //                                                                                //
 //--------------------------------------------------------------------------------//
 //  Revision 0.01 - File Created                                                  //
