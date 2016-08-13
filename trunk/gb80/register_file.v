8e2bb78237099afc1b18d2f205762ad80e8bf295
kdmillar <kdmillar13@gmail.com>
2016-08-12 23:40:40 -0400

    Updated the register file module prototype

--- trunk/gb80/register_file.v
+++ trunk/gb80/register_file.v
@@ -12,3 +12,18 @@
 //--------------------------------------------------------------------------------//
 //  Revision 0.01 - File Created                                                  //
 //--------------------------------------------------------------------------------//
+module register_file #(
+  parameter                          DATA_WIDTH = 8,
+  parameter                          ADDRESS_WIDTH = 3
+)(
+  input                              i_clk,
+  input                              i_reset,
+  input                              i_wr_en,
+  input                              i_rd_en,
+  input  [ADDRESS_WIDTH-1:0]         i_addr,
+  input  [DATA_WIDTH-1:0]            i_data,
+  output [DATA_WIDTH-1:0]            o_data
+);
+
+  always@ posedge(i_clk) begin : clkProcess
+    
